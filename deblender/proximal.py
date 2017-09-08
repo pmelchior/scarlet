b3458608244aad499a19a7436cf85afd02506a2a
fred3m <fredem@princeton.edu>
2017-09-08 09:36:48 -0400

    Check for odd rows/columns in build_monotonic
    When this operator is built for an image with an even number of
    rows and/or columns, the result is a catastrophic failure.
    This commit adds a check that the user has already projected
    the image to a space with an odd number of rows and columns.
    
    It also changes the name of the proximal operator, which
    will be useful in the future when implementing a version
    of monotonicity that doesn't just look at the radial
    direction, but at all directions coming from the peak.
    This future change may help model spiral galaxies
    more accurately.

--- deblender/proximal.py
+++ deblender/proximal.py
@@ -7,7 +7,7 @@ import numpy as np
 from . import operators
 from . import proximal_utils
 
-def prox_monotonic(X, step, seeks, ref_idx, dist_idx, thresh=0, prox_chain=None, **kwargs):
+def _prox_strict_monotonic(X, step, seeks, ref_idx, dist_idx, thresh=0, prox_chain=None, **kwargs):
     """Force an intensity profile to be monotonic
     """
     proximal_utils.prox_monotonic(X, step, seeks, ref_idx, dist_idx, thresh)
@@ -21,22 +21,25 @@ def prox_monotonic(X, step, seeks, ref_idx, dist_idx, thresh=0, prox_chain=None,
 def build_prox_monotonic(shape, seeks, prox_chain=None, thresh=0):
     """Build the prox_monotonic operator
     """
+    if not shape[0] % 2 or not shape[1] % 2:
+        err = "Shape must have an odd width and height, received shape {0}".format(shape)
+        raise ValueError(err)
     monotonicOp = operators.getRadialMonotonicOp(shape)
     _, refIdx = np.where(monotonicOp.toarray()==1)
     # Get the center pixels
-    px = (shape[1]-1) >> 1
-    py = (shape[0]-1) >> 1
+    cx = (shape[1]-1) >> 1
+    cy = (shape[0]-1) >> 1
     # Calculate the distance between each pixel and the peak
     x = np.arange(shape[1])
     y = np.arange(shape[0])
     X,Y = np.meshgrid(x,y)
-    X = X - px
-    Y = Y - py
+    X = X - cx
+    Y = Y - cy
     distance = np.sqrt(X**2+Y**2)
     # Get the indices of the pixels sorted by distance from the peak
     didx = np.argsort(distance.flatten())
     #update the strict proximal operators
-    return partial(prox_monotonic, seeks=seeks, ref_idx=refIdx.tolist(), dist_idx=didx.tolist(), prox_chain=prox_chain, thresh=thresh)
+    return partial(_prox_strict_monotonic, seeks=seeks, ref_idx=refIdx.tolist(), dist_idx=didx.tolist(), prox_chain=prox_chain, thresh=thresh)
 
 def prox_cone(X, step, G=None):
     """Exact projection of components of X onto cone defined by Gx >= 0"""
