b169ba7c6daf92f5436a95f2203ca03f1a59263a
Peter Melchior <peter.m.melchior@gmail.com>
2017-12-01 10:49:58 -0500

    finished splitting deblender.py into two files

--- scarlet/__init__.py
+++ scarlet/__init__.py
@@ -1,3 +1,4 @@
 # convenience: get vanilla NMF and deblend wrapper directly within scarlet
 from proxmin import nmf as nmf
-from .deblender import Source,Blend
+from .source import Source
+from .blend import Blend
