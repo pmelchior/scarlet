7a552a044faaa6bb26bfd5deee29d3fd3c7f3f2c
fred3m <fredem@princeton.edu>
2019-06-10 09:02:40 -0400

    Fix flake8 errors

--- scarlet/display.py
+++ scarlet/display.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Norm:
     """Base class for RGB normalization
     """
@@ -15,11 +16,13 @@ class Norm:
         """
         self.vmin = vmin
         self.vmax = vmax
+
     @property
     def vrange(self):
         """Returns range `vmax` - `vmin`
         """
         return self.vmax - self.vmin
+
     def __call__(self, img):
         """Apply the norm to RGB channels
 
@@ -33,6 +36,7 @@ class Norm:
         normalized image
         """
         pass
+
 
 class LinearNorm(Norm):
     """Linear norm
@@ -61,8 +65,9 @@ class LinearNorm(Norm):
         """
         return (img-self.vmin)/self.vrange
 
+
 class LinearPercentileNorm(LinearNorm):
-    def __init__(self, img, percentiles=[1,99]):
+    def __init__(self, img, percentiles=[1, 99]):
         """Create norm that is linear between lower and upper percentile of img
 
         Parameters
@@ -76,6 +81,7 @@ class LinearPercentileNorm(LinearNorm):
         assert len(percentiles) == 2
         vmin, vmax = np.percentile(img, percentiles)
         super().__init__(vmin=vmin, vmax=vmax)
+
 
 class AsinhNorm(Norm):
     def __init__(self, vmin=0, beta=1):
@@ -110,8 +116,9 @@ class AsinhNorm(Norm):
         I = _img.sum(axis=0)/3
         return np.ma.array(_img*np.arcsinh(I/self.beta)/I)
 
+
 class AsinhPercentileNorm(AsinhNorm):
-    def __init__(self, img, percentiles=[1,99]):
+    def __init__(self, img, percentiles=[1, 99]):
         """Create norm that is linear between lower and upper percentile of img
 
         Parameters
@@ -147,70 +154,71 @@ def img_to_channel(img, filter_weights=None, fill_value=0):
     RGB: numpy array with dtype float
     """
     # expand single img into cube
-    assert len(img.shape) in [2,3]
+    assert len(img.shape) in [2, 3]
     if len(img.shape) == 2:
         ny, nx = img.shape
         img_ = img.reshape(1, ny, nx)
     elif len(img.shape) == 3:
         img_ = img
     B = len(img_)
-    C = 3 # RGB
+    C = 3  # RGB
 
     # filterWeights: channel x band
     if filter_weights is None:
         filter_weights = np.zeros((C, B))
         if B == 1:
-            filter_weights[0,0] = filter_weights[1,0] = filter_weights[2,0] = 1
+            filter_weights[0, 0] = filter_weights[1, 0] = filter_weights[2, 0] = 1
         if B == 2:
-            filter_weights[0,1] = 0.667
-            filter_weights[1,1] = 0.333
-            filter_weights[1,0] = 0.333
-            filter_weights[2,0] = 0.667
+            filter_weights[0, 1] = 0.667
+            filter_weights[1, 1] = 0.333
+            filter_weights[1, 0] = 0.333
+            filter_weights[2, 0] = 0.667
             filter_weights /= 0.667
         if B == 3:
-            filter_weights[0,2] = 1
-            filter_weights[1,1] = 1
-            filter_weights[2,0] = 1
+            filter_weights[0, 2] = 1
+            filter_weights[1, 1] = 1
+            filter_weights[2, 0] = 1
         if B == 4:
-            filter_weights[0,3] = 1
-            filter_weights[0,2] = 0.333
-            filter_weights[1,2] = 0.667
-            filter_weights[1,1] = 0.667
-            filter_weights[2,1] = 0.333
-            filter_weights[2,0] = 1
+            filter_weights[0, 3] = 1
+            filter_weights[0, 2] = 0.333
+            filter_weights[1, 2] = 0.667
+            filter_weights[1, 1] = 0.667
+            filter_weights[2, 1] = 0.333
+            filter_weights[2, 0] = 1
             filter_weights /= 1.333
         if B == 5:
-            filter_weights[0,4] = 1
-            filter_weights[0,3] = 0.667
-            filter_weights[1,3] = 0.333
-            filter_weights[1,2] = 1
-            filter_weights[1,1] = 0.333
-            filter_weights[2,1] = 0.667
-            filter_weights[2,0] = 1
+            filter_weights[0, 4] = 1
+            filter_weights[0, 3] = 0.667
+            filter_weights[1, 3] = 0.333
+            filter_weights[1, 2] = 1
+            filter_weights[1, 1] = 0.333
+            filter_weights[2, 1] = 0.667
+            filter_weights[2, 0] = 1
             filter_weights /= 1.667
         if B == 6:
-            filter_weights[0,5] = 1
-            filter_weights[0,4] = 0.667
-            filter_weights[0,3] = 0.333
-            filter_weights[1,4] = 0.333
-            filter_weights[1,3] = 0.667
-            filter_weights[1,2] = 0.667
-            filter_weights[1,1] = 0.333
-            filter_weights[2,2] = 0.333
-            filter_weights[2,1] = 0.667
-            filter_weights[2,0] = 1
+            filter_weights[0, 5] = 1
+            filter_weights[0, 4] = 0.667
+            filter_weights[0, 3] = 0.333
+            filter_weights[1, 4] = 0.333
+            filter_weights[1, 3] = 0.667
+            filter_weights[1, 2] = 0.667
+            filter_weights[1, 1] = 0.333
+            filter_weights[2, 2] = 0.333
+            filter_weights[2, 1] = 0.667
+            filter_weights[2, 0] = 1
             filter_weights /= 2
     else:
         assert filter_weights.shape == (3, len(img))
 
     # map bands onto RGB channels
-    _,ny,nx = img_.shape
-    rgb = np.dot(filter_weights, img_.reshape(B,-1)).reshape(3,ny,nx)
+    _, ny, nx = img_.shape
+    rgb = np.dot(filter_weights, img_.reshape(B, -1)).reshape(3, ny, nx)
 
     if hasattr(rgb, "mask"):
         rgb = rgb.filled(fill_value)
 
     return rgb
+
 
 def channel_to_rgb(channels, norm):
     """Convert RGB channel values to [0..255]
@@ -232,13 +240,15 @@ def channel_to_rgb(channels, norm):
     rgb = norm(channels)
 
     # truncate at 0 and 1
-    rgb[rgb < 0] = 0 # looks better but is inaccurate because colors are not treated the same
-    # normalizing by a uniform maximum is critical for the color of saturated regions
+    rgb[rgb < 0] = 0  # looks better but is inaccurate because colors are not treated the same
+    # normalizing by a uniform maximum is critical
+    # for the color of saturated regions
     rgb_max = rgb.max(axis=0)
     rgb[:, rgb_max > 1] /= rgb_max[rgb_max > 1]
 
     maxRGB = 255
     return np.dstack((rgb * maxRGB)).astype(np.uint8)
+
 
 def img_to_rgb(img, filter_weights=None, fill_value=0, norm=None):
     """Convert images to normalized RGB.
