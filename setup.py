b8c0f9fb6af448f3f3938a7369b22a500ccd13c0
fred3m <fredem@princeton.edu>
2019-06-23 23:51:52 -0400

    Require proxmin v0.5.5 to use inline update fix

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ class BuildExt(build_ext):
         build_ext.build_extensions(self)
 
 
-install_requires = ['numpy', 'proxmin>=0.5.4', 'autograd']
+install_requires = ['numpy', 'proxmin>=0.5.5', 'autograd']
 # Only require the pybind11 and peigen packages if
 # the C++ headers are not already installed
 if pybind11_path is None:
