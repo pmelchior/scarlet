4ea7291d7805135aa7c3bf08774a85295bb0d1a7
herjy <remy.joseph11@gmail.com>
2019-07-05 11:32:14 -0400

    cprofile

--- scarlet/observation.py
+++ scarlet/observation.py
@@ -204,7 +204,7 @@ class Observation():
             self.slices = tuple(([slice(s) for s in shape]))
 
             # Now we setup the parameters for the psf -> kernel FFTs
-            shape = np.array(model_frame.psfs.shape) 
+            shape = np.array(model_frame.psfs.shape)
             shape[1:] += np.array(self.frame.psfs[0].shape)
 
             _fftpack_shape = [fftpack.helper.next_fast_len(d) for d in shape[1:]]
