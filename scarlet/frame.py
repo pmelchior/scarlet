rotation fixed

scarlet/frame.py
@@ -2,7 +2,6 @@ import numpy as np
 from .psf import PSF
 from .bbox import Box
 from . import interpolation
-from . import resampling
 import logging
 
 logger = logging.getLogger("scarlet.frame")
@@ -150,15 +149,16 @@ class Frame(Box):
                 psf_size = get_psf_size(psf)*h_temp
                 if (fat_psf_size is None) or (psf_size > fat_psf_size):
                     fat_psf_size = psf_size
+                    fat_pixel_size = psf_size/h_temp
                 if (obs_id is None) or (c == obs_id):
                     if (target_psf is None) and ((small_psf_size is None) or (psf_size < small_psf_size)):
+                        small_psf_size = psf_size
                         target_psf_temp = PSF(psf[np.newaxis, :, :])
                         psf_h = h_temp
 
-
         # Find a reference observation. Either provided by obs_id or as the observation with the smallest pixel
         if obs_id is None:
-            obs_ref = observations[int((pix_tab == np.min(pix_tab))[0])]
+            obs_ref = observations[np.where(pix_tab == np.min(pix_tab))[0][0]]
         else:
             #Frame defined from obs_id
             obs_ref = observations[obs_id]
@@ -171,16 +171,11 @@ class Frame(Box):
         # If needed and psf is not provided: interpolate psf to smallest pixel
         if target_psf is None:
             # If the reference PSF is not at the highest pixel resolution, make it!
-            if psf_h > interpolation.get_pixel_size(interpolation.get_affine(target_wcs)):
-                coord_lr = [range(target_psf_temp.shape[-2]), range(target_psf_temp.shape[-1])]
-                ny_hr, nx_hr = target_psf_temp.shape[-2] * psf_h / h, target_psf_temp.shape[-1] * psf_h / h
-                if (ny_hr % 2) == 0:
-                    ny_hr += 1
-                if (nx_hr % 2) == 0:
-                    nx_hr += 1
-                coord_hr = [range(ny_hr), range(nx_hr)]
-                angle, h = interpolation.get_angles(target_wcs, obs.wcs)
-                target_psf = PSF(interpolation.sinc_interp(target_psf_temp, coord_hr, coord_lr, angle=angle))
+            if psf_h > h:
+                angle, h = interpolation.get_angles(target_wcs, obs.frame.wcs)
+                target_psf = PSF(interpolation.sinc_interp_inplace(target_psf_temp, psf_h, h, angle))
+            else:
+                target_psf = target_psf_temp
 
         # Matching observations together so as to create a common frame
         obs_coords = []
@@ -190,8 +185,7 @@ class Frame(Box):
             if (obs.frame.wcs is not target_wcs) and (type(obs) is not 'LowResObservation'):
                 observations[c] = obs.make_LowRes()
 
-            # Is the angle larger than machine precision?
-            obs_coord = resampling.get_to_common_frame(obs, target_wcs)
+            obs_coord = obs.match_coordinates(target_wcs)
             if np.min(obs_coord[0]) < y_min:
                 y_min = np.min(obs_coord[0])
             if np.min(obs_coord[1]) < x_min:
@@ -206,38 +200,45 @@ class Frame(Box):
         if fat_psf_size % 2 == 0:
             fat_psf_size += 1
         fat_psf_size.astype(int)
-        ny = (y_max - y_min + 1 + fat_psf_size).astype(int)
-        nx = (x_max - x_min + 1 + fat_psf_size).astype(int)
+        ny = (y_max - y_min + 1 + fat_pixel_size).astype(int)
+        nx = (x_max - x_min + 1 + fat_pixel_size).astype(int)
 
         footprint = np.zeros((ny,nx))
         for coord in obs_coords:
-            footprint[(coord[0] - y_min+ fat_psf_size/2).astype(int),
-                      (coord[1] - x_min+ fat_psf_size/2).astype(int)] += 1
+            footprint[(coord[0] - y_min + fat_pixel_size/2).astype(int),
+                      (coord[1] - x_min + fat_pixel_size/2).astype(int)] += 1
 
         if coverage is 'union':
             coord_frame = np.where(footprint != 0)
 
         elif coverage is 'intersection':
-            coord_frame = np.where(footprint == np.max(footprint))
+            coord_frame = (np.where(footprint == np.max(footprint)))
+        y_min = np.min(coord_frame[0])
+        x_min = np.min(coord_frame[1])
+        y_max = np.max(coord_frame[0])
+        x_max = np.max(coord_frame[1])
+
+        ny = (y_max - y_min + 1 + fat_pixel_size).astype(int)
+        nx = (x_max - x_min + 1 + fat_pixel_size).astype(int)
 
         frame_shape =(len(channels), ny, nx)
+        if np.size(target_wcs.array_shape) == 3:
+            target_wcs.array_shape = frame_shape
+            target_wcs.crval = target_wcs.all_pix2world(x_min, y_min, 0, 0, ra_dec_order=True)
+            target_wcs.crpix = [0, 0, 0]
+
+        if np.size(target_wcs.array_shape) == 2:
+            target_wcs.array_shape = frame_shape[-2:]
+            target_wcs.crval = target_wcs.all_pix2world(x_min, y_min, 0, ra_dec_order=True)
+            target_wcs.crpix = [0, 0]
+
         frame = Frame(frame_shape, wcs=target_wcs, psfs=target_psf, channels=channels)
+
         # Match observations to this frame
         for obs in observations:
-            obs.match(frame, coord_frame)
+            obs.match(frame)
 
-        return
-
-
-def get_boundaries(frame_wcs, observation):
-    """ Extracts the boundaries of an observation relative to a given frame
-
-    Parameters:
-        frame_wcs: wcs
-            reference frame for matching coordinates
-        observations: `Observation` object
-            observation which boundaries we want to extract.
-    """
+        return frame
 
 
 def get_psf_size(psf):
@@ -256,14 +257,14 @@ def get_psf_size(psf):
             radius of the area inside 3 sigma around the center in pixels
     """
     # Normalisation by maximum
-    psf /= np.max(psf)
+    psf_frame = psf/np.max(psf)
 
     # Pixels in the FWHM set to one, others to 0:
-    psf[psf>0.5] = 1
-    psf[psf<=0.5] = 0
+    psf_frame[psf_frame>0.5] = 1
+    psf_frame[psf_frame<=0.5] = 0
 
     # Area in the FWHM:
-    area = np.sum(psf)
+    area = np.sum(psf_frame)
 
     # Diameter of this area
     d = 2*(area/np.pi)**0.5

scarlet/interpolation.py
@@ -2,7 +2,6 @@ import numpy as np
 from .cache import Cache
 from . import fft
 
-
 def get_projection_slices(image, shape, yx0=None):
     """Get slices needed to project an image
 
@@ -385,7 +384,7 @@ def get_angles(frame_wcs, model_wcs):
     sin_rot = np.cross(self_framevector, model_framevector)
     # cos of the angle. (normalised scalar product)
     cos_rot = np.dot(self_framevector, model_framevector)
-    return (cos_rot, sin_rot), h
+    return [cos_rot, sin_rot], h
 
 def sinc_interp(images, coord_hr, coord_lr, angle=None, padding=3):
     """
@@ -413,7 +412,7 @@ def sinc_interp(images, coord_hr, coord_lr, angle=None, padding=3):
     assert hy != 0
     assert hx != 0
 
-    if angle is None:
+    if (angle is None) or (1 - angle[0] < np.finfo(float).eps):
         result = [
             np.dot(
                 np.dot(
@@ -438,8 +437,8 @@ def sinc_interp(images, coord_hr, coord_lr, angle=None, padding=3):
     shifter_y, shifter_x = mk_shifter(fft_shape)
 
     #Shifts values
-    shift_y = np.exp(shifter_y[np.newaxis, :] * (-(y_hr[:, np.newaxis]) * cos))
-    shift_x = np.exp(shifter_x[np.newaxis, :] * (-(y_hr[:, np.newaxis]) * sin))
+    shift_y = np.exp(shifter_y[np.newaxis, :] * (- (y_hr[:, np.newaxis]) * cos))
+    shift_x = np.exp(shifter_x[np.newaxis, :] * (- (y_hr[:, np.newaxis]) * sin))
     #Apply shifts
 
     result_fft = X_fft[:, np.newaxis, :, :] * shift_y[np.newaxis, :, :, np.newaxis]
@@ -463,6 +462,43 @@ def sinc_interp(images, coord_hr, coord_lr, angle=None, padding=3):
     result = (result_y * shx[np.newaxis, np.newaxis, :, :]).sum(axis=-1)
 
     return result
+
+
+def sinc_interp_inplace(image, h_image, h_target, angle):
+    """ In place interpolation of a cube of images
+
+    Performs interpolation from a grid defined by the grid of `image` to a grid spanning the same physical area scaled
+    by a factor `h` and rotated by `angle` radians. The center for the rotation is the central pixel of the image.
+    This procedure is advised for odd-sized images only.
+
+    Parameters
+    ----------
+    image: `ndarray`
+        Cube of images with shape BxNyxNx with B the number of bands and NyxNx, the number of pixels
+    h_image: `float`
+        Phisical scale of a pixel in image
+    h_target: `float`
+        Physical scale of the target pixel to which to interpolate
+    angle: float
+        angle between the grid of image and the target grid where to interpolate
+    Returns
+    -------
+    interp_image: `ndarray`
+        interpolated image
+    """
+    assert len(image.shape) == 3, "images should be provided as a cube. If only one image is provided, " \
+                                  "image should be a cube with image.shape[0] = 1"
+    ny_lr, nx_lr = image.shape[-2:]
+    coord_lr = np.array([np.array(range(ny_lr)) - (ny_lr-1)/2, np.array(range(nx_lr))-(nx_lr-1)/2])
+    ny_hr, nx_hr = (image.shape[-2] * h_image / h_target).astype(int), \
+                   (image.shape[-1] * h_image / h_target).astype(int)
+    if (ny_hr % 2) == 0:
+        ny_hr += 1
+    if (nx_hr % 2) == 0:
+        nx_hr += 1
+    coord_hr = np.array([np.array(range(ny_hr.astype(int)))-(ny_hr-1)/2, np.array(range(nx_hr.astype(int)))-(nx_hr-1)/2])
+
+    return sinc_interp(image, coord_hr, coord_lr, angle=angle)
 
 
 def fft_resample(img, dy, dx, kernel=lanczos, **kwargs):

scarlet/observation.py
@@ -6,7 +6,6 @@ from . import fft
 from . import resampling
 from .bbox import Box
 
-
 class Observation:
     """Data and metadata for a single set of observations
 
@@ -28,7 +27,7 @@ class Observation:
     """
 
     def __init__(
-        self, images, psfs=None, weights=None, wcs=None, channels=None, padding=10
+            self, images, psfs=None, weights=None, wcs=None, channels=None, padding=10
     ):
         """Create an Observation
 
@@ -62,10 +61,28 @@ class Observation:
         else:
             self.weights = np.ones(images.shape)
         assert (
-            self.weights.shape == self.images.shape
+                self.weights.shape == self.images.shape
         ), "Weights needs to have same shape as images"
 
         self._padding = padding
+
+    def match_coordinates(self, wcs):
+        """ Matches the coordinates of the pixels in the observation to a different wcs.
+        This method is mostly used for child class LowResObservations, but having it here simplifies the writing when
+        matching frames.
+
+        Parameters
+        ----------
+        wcs: WCS
+            wcs of the frame to witch the observation's pixels are to be matched
+
+        Returns
+        -------
+        coords: array
+            coordinates of the pixels in the observation matched to the wcs
+        """
+        self.coord_frame = resampling.get_to_common_frame(self, wcs)
+        return self.coord_frame
 
     def match(self, model_frame):
         """Match the frame of `Blend` to the frame of this observation.
@@ -92,7 +109,6 @@ class Observation:
         else:
             assert self.frame.channels is not None and model_frame.channels is not None
             cmin = list(model_frame.channels).index(self.frame.channels[0])
-            cmax = list(model_frame.channels).index(self.frame.channels[-1])
             origin = (cmin, *yx0)
         self.bbox = Box(shape, origin=origin)
         self.slices = self.bbox.slices_for(model_frame.shape)
@@ -166,8 +182,8 @@ class Observation:
         # full likelihood is sum over all data samples: pixel in images
         # NOTE: this assumes that all pixels are used in likelihood!
         log_norm = (
-            np.prod(images_.shape) / 2 * np.log(2 * np.pi)
-            + np.sum(np.log(1 / self.weights)) / 2
+                np.prod(images_.shape) / 2 * np.log(2 * np.pi)
+                + np.sum(np.log(1 / self.weights)) / 2
         )
 
         return log_norm + np.sum(weights_ * (model_ - images_) ** 2) / 2
@@ -177,21 +193,21 @@ class Observation:
 
         """
         return LowResObservation(self.images,
-                                 psfs = self.frame._psfs,
-                                 weights = self.weights,
-                                 wcs = self.frame.wcs,
-                                 channels= self.frame.channels)
+                                 psfs=self.frame._psfs,
+                                 weights=self.weights,
+                                 wcs=self.frame.wcs,
+                                 channels=self.frame.channels)
 
 
 class LowResObservation(Observation):
     def __init__(
-        self,
-        images,
-        wcs=None,
-        psfs=None,
-        weights=None,
-        channels=None,
-        padding=3,
+            self,
+            images,
+            wcs=None,
+            psfs=None,
+            weights=None,
+            channels=None,
+            padding=3,
     ):
 
         assert wcs is not None, "WCS is necessary for LowResObservation"
@@ -206,7 +222,7 @@ class LowResObservation(Observation):
             padding=padding,
         )
 
-    def match_psfs(self, psf_hr, wcs_hr, angle):
+    def match_psfs(self, psf_hr, wcs_hr):
         """psf matching between different dataset
         Matches PSFS at different resolutions by interpolating psf_lr on the same grid as psf_hr
         Parameters
@@ -229,46 +245,14 @@ class LowResObservation(Observation):
             low resolution psf at matching size and resolution
         """
 
-        psf_lr = self.frame.psf.image
+        psf_lr = self.frame._psfs.image
         wcs_lr = self.frame.wcs
 
-        _, ny_hr, nx_hr = psf_hr.shape
-        npsf_bands, ny_lr, nx_lr = psf_lr.shape
+        h_lr = interpolation.get_pixel_size(interpolation.get_affine(wcs_lr))
+        h_hr = interpolation.get_pixel_size(interpolation.get_affine(wcs_hr))
 
-        # Createsa wcs for psf centered around the frame center
-        psf_wcs_hr = wcs_hr.deepcopy()
-        psf_wcs_lr = wcs_lr.deepcopy()
-
-        if psf_wcs_hr.naxis == 2:
-            psf_wcs_hr.wcs.crval = 0.0, 0.0
-            psf_wcs_hr.wcs.crpix = ny_hr / 2.0 + 1, nx_hr / 2.0 + 1
-        elif psf_wcs_hr.naxis == 3:
-            psf_wcs_hr.wcs.crval = 0.0, 0.0, 0.0
-            psf_wcs_hr.wcs.crpix = ny_hr / 2 + 1, nx_hr / 2.0 + 1, 0.0
-        if psf_wcs_lr.naxis == 2:
-            psf_wcs_lr.wcs.crval = 0.0, 0.0
-            psf_wcs_lr.wcs.crpix = ny_lr / 2.0 + 1, nx_lr / 2.0 + 1
-        elif psf_wcs_lr.naxis == 3:
-
-            psf_wcs_lr.wcs.crval = 0.0, 0.0, 0.0
-            psf_wcs_lr.wcs.crpix = ny_lr / 2.0 + 1, nx_lr / 2.0 + 1, 0
-
-        pcoordlr_lr, pcoordlr_hr, coordover_hr = resampling.match_patches(
-            psf_hr.shape,
-            psf_lr.data.shape[1:],
-            psf_wcs_hr,
-            psf_wcs_lr,
-            isrot=False
-        )
-
-        psf_valid = psf_lr[
-            :,
-            pcoordlr_lr[0].min() : pcoordlr_lr[1].max() + 1,
-            pcoordlr_lr[1].min() : pcoordlr_lr[1].max() + 1,
-        ]
-        psf_match_lr = interpolation.sinc_interp(
-            psf_valid, coordover_hr, pcoordlr_hr, angle=angle
-        )
+        angle, h_ratio = interpolation.get_angles(wcs_hr, wcs_lr)
+        psf_match_lr = interpolation.sinc_interp_inplace(psf_lr, h_lr, h_hr, angle)
 
         psf_hr /= np.sum(psf_hr)
         psf_match_lr /= np.sum(psf_match_lr)
@@ -293,11 +277,11 @@ class LowResObservation(Observation):
         whr = model_frame.wcs
 
         # Reference PSF
-        _target = model_frame.psf.image
+        _target = model_frame._psfs.image
 
         # Computes spatially matching observation and target psfs. The observation psf is also resampled \\
         # to the model frame resolution
-        new_target, observed_psfs = self.match_psfs(_target, whr, angle)
+        new_target, observed_psfs = self.match_psfs(_target, whr)
         diff_psf = fft.match_psfs(fft.Fourier(observed_psfs), fft.Fourier(new_target))
 
         return diff_psf
@@ -342,9 +326,9 @@ class LowResObservation(Observation):
             # Shift along the x-axis
             if 1 in axes:
                 # Fourier shift
-                shishift = np.exp(shifter[1][np.newaxis, :] * shifts[1][:,np.newaxis])
+                shishift = np.exp(shifter[1][np.newaxis, :] * shifts[1][:, np.newaxis])
                 imgs_shiftfft = imgs_shiftfft * shishift[np.newaxis, :, np.newaxis, :]
-                fft_axes = np.array(axes)+len(imgs_shiftfft.shape)-2
+                fft_axes = np.array(axes) + len(imgs_shiftfft.shape) - 2
             inv_shape = tuple(imgs_shiftfft.shape[:2]) + tuple(transformed_shape)
 
 
@@ -353,21 +337,22 @@ class LowResObservation(Observation):
             shishift = np.exp(shifter[1][:, np.newaxis] * shifts[1][np.newaxis, :])
             imgs_shiftfft = imgs_fft[:, :, :, np.newaxis] * shishift[np.newaxis, np.newaxis, :, :]
             inv_shape = tuple([imgs_shiftfft.shape[0]]) + tuple(transformed_shape) + tuple([imgs_shiftfft.shape[-1]])
-            fft_axes = [len(imgs_shiftfft.shape)-2]
+            fft_axes = [len(imgs_shiftfft.shape) - 2]
 
         # Inverse Fourier transform.
         # The n-dimensional transform could pose problem for very large images
         op = fft.Fourier.from_fft(imgs_shiftfft, fft_shape, inv_shape, fft_axes).image
         return op
 
-    def match(self, model_frame, coord):
+    def match(self, model_frame):
         """ matches the observation to a frame
 
         Parameters
         ----------
         model_frame: `Frame`
-
-
+            Frame to match to the observation
+        coord: `array`
+            coordinates of the pixels in the frame to fit
         """
 
         if self.frame.dtype != model_frame.dtype:
@@ -377,21 +362,13 @@ class LowResObservation(Observation):
             if self.frame._psfs is not None:
                 self.frame._psfs.update_dtype(model_frame.dtype)
 
-        # channels of model that are represented in this observation
-        self._band_slice = slice(None)
-        if self.frame.channels is not model_frame.channels:
-            bmin = model_frame.channels.index(self.frame.channels[0])
-            bmax = model_frame.channels.index(self.frame.channels[-1])
-            self._band_slice = slice(bmin, bmax+1)
-
         self.angle, self.h = interpolation.get_angles(self.frame.wcs, model_frame.wcs)
 
         # Is the angle larger than machine precision?
         self.isrot = (np.abs(self.angle[1]) ** 2) > np.finfo(float).eps
         if not self.isrot:
-            self.angle[1] = 0
-            self.angle[0] = 1
-            angle = None
+            self.angle = None
+
 
         # Get pixel coordinates in each frame.
         coord_lr, coord_hr, coordhr_over = resampling.match_patches(
@@ -399,11 +376,9 @@ class LowResObservation(Observation):
             self.frame.shape,
             model_frame.wcs,
             self.frame.wcs,
-            isrot=self.isrot,
-            coverage = coverage
+            isrot=self.isrot
         )
-
-        # shape of the low resolutino image in the intersection or union
+        # shape of the low resolution image in the intersection or union
         self.lr_shape = (
             np.max(coord_lr[0]) - np.min(coord_lr[0]) + 1,
             np.max(coord_lr[1]) - np.min(coord_lr[1]) + 1,
@@ -429,7 +404,7 @@ class LowResObservation(Observation):
             np.array(range(model_frame.Nx)),
         )
 
-        diff_psf = self.build_diffkernel(model_frame, angle)
+        diff_psf = self.build_diffkernel(model_frame, self.angle)
 
         # 1D convolutions convolutions of the model are done along the smaller axis, therefore,
         # psf is convolved along the frame's longer axis.
@@ -445,31 +420,25 @@ class LowResObservation(Observation):
         )
         self.diff_psf = fft.Fourier(fft._pad(diff_psf.image, self._fft_shape, axes=(1, 2)))
 
-        center_y = np.int(self._fft_shape[0]/2.-(self._fft_shape[0]-model_frame.Ny)/2.) - \
+        center_y = np.int(self._fft_shape[0] / 2. - (self._fft_shape[0] - model_frame.Ny) / 2.) - \
                    ((self._fft_shape[0] % 2) != 0) * ((model_frame.Ny % 2) == 0)
-        center_x = np.int(self._fft_shape[1]/2.-(self._fft_shape[1]-model_frame.Nx)/2.) - \
+        center_x = np.int(self._fft_shape[1] / 2. - (self._fft_shape[1] - model_frame.Nx) / 2.) - \
                    ((self._fft_shape[1] % 2) != 0) * ((model_frame.Nx % 2) == 0)
         if self.isrot:
 
             # Unrotated coordinates:
             Y_unrot = (
-                (coord_hr[0] - center_y) * self.angle[0]
-                - (coord_hr[1] - center_x) * self.angle[1]
+                    (coord_hr[0] - center_y) * self.angle[0]
+                    - (coord_hr[1] - center_x) * self.angle[1]
             ).reshape(self.lr_shape)
             X_unrot = (
-                (coord_hr[1] - center_x) * self.angle[0]
-                + (coord_hr[0] - center_y) * self.angle[1]
+                    (coord_hr[1] - center_x) * self.angle[0]
+                    + (coord_hr[0] - center_y) * self.angle[1]
             ).reshape(self.lr_shape)
 
             # Removing redundancy
             self.Y_unrot = Y_unrot[:, 0]
             self.X_unrot = X_unrot[0, :]
-
-            import matplotlib.pyplot as plt
-
-            plt.plot(coord_lr[0], coord_lr[1],'or')
-            plt.plot(self.Y_unrot, self.X_unrot, 'ob')
-            plt.plot(coord_hr[0], coord_hr[1], 'xg')
 
             if self.small_axis:
                 self.shifts = np.array([self.Y_unrot * self.angle[0], self.Y_unrot * self.angle[1]])
@@ -503,8 +472,7 @@ class LowResObservation(Observation):
         # Computes the resampling/convolution matrix
         resconv_op = self.sinc_shift(self.diff_psf, self.shifts, axes)
 
-
-        self._resconv_op = np.array(resconv_op, dtype=self.frame.dtype)*self.h**2
+        self._resconv_op = np.array(resconv_op, dtype=self.frame.dtype) * self.h ** 2
 
         if self.isrot:
             self._resconv_op = self._resconv_op.reshape(*self._resconv_op.shape[:2], -1)
@@ -515,7 +483,6 @@ class LowResObservation(Observation):
         else:
             self._resconv_op = self._resconv_op.reshape(self._resconv_op.shape[0], -1, self._resconv_op.shape[-1])
             return self
-
 
     def _render(self, model):
         """Resample and convolve a model in the observation frame
@@ -541,7 +508,7 @@ class LowResObservation(Observation):
             axes = [int(self.small_axis) + 1]
 
         model_conv = self.sinc_shift(model_, -self.other_shifts, axes)
-        #Transposes are all over the place to make arrays F-contiguous
+        # Transposes are all over the place to make arrays F-contiguous
         if self.isrot:
             if self.small_axis:
                 model_conv = model_conv.reshape(*model_conv.shape[:2], -1)
@@ -554,10 +521,8 @@ class LowResObservation(Observation):
                     model_image.append((self._resconv_op[c] @ model_conv[c].T).T)
                 return np.array(model_image, dtype=self.frame.dtype)
 
-
-
         if self.small_axis:
-            model_conv = model_conv.reshape(model_conv.shape[0],-1,model_conv.shape[-1])
+            model_conv = model_conv.reshape(model_conv.shape[0], -1, model_conv.shape[-1])
             for c in range(self.frame.C):
                 model_image.append((model_conv[c].T @ self._resconv_op[c].T).T)
             return np.array(model_image, dtype=self.frame.dtype)
@@ -579,7 +544,7 @@ class LowResObservation(Observation):
             `model` mapped into the observation frame
         """
         image_model = np.zeros(self.frame.shape)
-        image_model[self.slices] = self._render(model)
+        image_model[:,self.slices[-2], self.slices[-1]] = self._render(model)
         return image_model
 
     def get_loss(self, model):
@@ -595,13 +560,13 @@ class LowResObservation(Observation):
         """
 
         model_ = self._render(model)
-        images_ = self.images[self.slices]
-        weights_ = self.weights[self.slices]
+        images_ = self.images[:,self.slices[-2], self.slices[-1]]
+        weights_ = self.weights[:,self.slices[-2], self.slices[-1]]
 
         # properly normalized likelihood
         log_norm = (
-            np.prod(images_.shape) / 2 * np.log(2 * np.pi)
-            + np.sum(np.log(1 / weights_)) / 2
+                np.prod(images_.shape) / 2 * np.log(2 * np.pi)
+                + np.sum(np.log(1 / weights_)) / 2
         )
 
         return log_norm + 0.5 * np.sum(weights_ * (model_ - images_) ** 2)

scarlet/resampling.py
@@ -12,7 +12,7 @@ def get_to_common_frame(obs, frame_wcs):
         a wcs that gives the mapping between pixel coordinates and sky coordinates for a given frame
     Returns
     -------
-        coord: `array`
+        coord_obs_frame: `tuple`
             Coordinates of the observations's pixels in the frame of the provided wcs
     """
     c, ny, nx = obs.frame.shape
@@ -35,8 +35,6 @@ def get_to_common_frame(obs, frame_wcs):
 
     coord = (Y,X)
     return coord
-
-
 
 
 
@@ -137,23 +135,14 @@ def match_patches(shape_hr, shape_lr, wcs_hr, wcs_lr, isrot = True, coverage  = 
     if np.sum(over_lr) == 0:
         raise SourceInitError
 
-    if coverage is "intersection":
-        # Coordinates of low resolution pixels in the intersection at low resolution:
-        ylr_lr = Y_lr[(over_lr == 1)]
-        xlr_lr = X_lr[(over_lr == 1)]
-        coordlr_lr = (ylr_lr, xlr_lr)
-        # Coordinates of low resolution pixels in the intersection at high resolution:
-        ylr_hr = Y_hr[(over_lr == 1)]
-        xlr_hr = X_hr[(over_lr == 1)]
+    # Coordinates of low resolution pixels in the intersection at low resolution:
+    ylr_lr = Y_lr[(over_lr == 1)]
+    xlr_lr = X_lr[(over_lr == 1)]
+    coordlr_lr = (ylr_lr, xlr_lr)
+    # Coordinates of low resolution pixels in the intersection at high resolution:
+    ylr_hr = Y_hr[(over_lr == 1)]
+    xlr_hr = X_hr[(over_lr == 1)]
 
-        coordlr_hr = (ylr_hr, xlr_hr)
-
-    elif coverage is "union":
-
-        # Coordinates of low resolution pixels at low resolution:
-        coordlr_lr = (Y_lr, X_lr)
-
-        # Coordinates of low resolution pixels at high resolution:
-        coordlr_hr = (Y_hr, X_hr)
+    coordlr_hr = (ylr_hr, xlr_hr)
 
     return coordlr_lr, coordlr_hr, coordhr_hr