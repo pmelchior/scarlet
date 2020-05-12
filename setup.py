4ac9d70bf503e275f41f735f731e50fed9a9617c
fred3m <fredem@princeton.edu>
2020-05-12 14:10:09 -0400

    fix version correctly

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if "EIGEN_DIR" in os.environ:
 # Use the firt 7 digits of the git hash to set the version
 version_root = "1.0"
 try:
-    __version__ = version_root+'.'+subprocess.check_output(['git', 'rev-parse', 'HEAD'])[:7].decode("utf-8")
+    __version__ = version_root+'.dev0+'+subprocess.check_output(['git', 'rev-parse', 'HEAD'])[:7].decode("utf-8")
 except:
     __version__ = version_root
 
