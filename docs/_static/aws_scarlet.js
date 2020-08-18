904b67c590e025db1bd60b063c661b222d377fe0
fred3m <fredem@princeton.edu>
2020-08-18 17:01:45 -0400

    Interactive tests checkpoint

--- docs/_static/aws_scarlet.js
+++ docs/_static/aws_scarlet.js
@@ -1,11 +1,14 @@
 // The client that connects to AWS DynamoDB
-var docClient
+let docClient
 
 // Blend ids for the current
-var blendIds = [];
+let blendIds = [];
 
 // Branches that have been analyzed
-var branches = [];
+let branches = [];
+
+// Merged branches
+let merged_branches = [];
 
 
 function get_blends(set_id, callback){
@@ -21,13 +24,13 @@ function get_blends(set_id, callback){
     }
 
     docClient.query(params, function(err, data){
-        var blend_data = data["Items"];
+        let blend_data = data["Items"];
         if (err) {
             console.log(err);
         } else {
             // Store the blend id's
             blendIds = [];
-            for(var i=0; i<blend_data.length; i++){
+            for(let i=0; i<blend_data.length; i++){
                 blendIds.push(blend_data[i]["blend_id"]);
             }
             console.log("blend ids", blendIds);
@@ -38,7 +41,7 @@ function get_blends(set_id, callback){
 
 
 function get_branches(callback){
-    var params = {
+    let params = {
         TableName: "scarlet_branches",
     };
 
@@ -47,8 +50,8 @@ function get_branches(callback){
             console.log(err);
         } else {
             // Initialize the dropdown buttons
-            var branch_data = data["Items"];
-            for(var i=0; i<branch_data.length; i++){
+            let branch_data = data["Items"];
+            for(let i=0; i<branch_data.length; i++){
                 branches.push(branch_data[i]["branch"]);
             }
             console.log("branches", branches);
@@ -58,12 +61,54 @@ function get_branches(callback){
 }
 
 
+function get_merged_branches(callback){
+    let params = {
+        TableName: "scarlet_merged",
+    };
+
+    docClient.scan(params, function(err, data){
+        if (err) {
+            console.log(err);
+        } else {
+            // Initialize the dropdown buttons
+            let branch_data = data["Items"];
+            branch_data.sort(function(a,b){
+                if(a["merge_order"] < b["merge_order"]){
+                    return -1;
+                } else if(a["merge_order"] > b["merge_order"]){
+                    return 1;
+                } else {
+                    console.log("Invalid merge order, received duplicate values for", a, b);
+                }
+            })
+
+            for(let i=0; i<branch_data.length; i++){
+                let branch = branch_data[i]["branch"];
+                // Only add branches that have been processed
+                if(branches.includes(branch)){
+                    merged_branches.push(branch);
+                }
+            }
+            console.log("merged branches", merged_branches);
+            callback();
+        }
+    });
+}
+
+
 function initBranches(options){
-    for(var i=0; i<options.length; i++){
-        var $select = $("#"+options[i]);
-        for(var j=0; j<branches.length; j++){
-            var branch = branches[j];
-            $select.append("<option value='"+branch+"'>"+branch+"</option>")
+    for(let i=0; i<options.length; i++){
+        let $select = $("#"+options[i]);
+        for(let j=0; j<branches.length; j++){
+            let branch = branches[j];
+            // Show the unmerged branches first
+            if(!merged_branches.includes(branch) && (branch !== "master")){
+                $select.append("<option value='"+branch+"'>"+branch+"</option>")
+            }
+        }
+        for(let j=0; j<merged_branches.length; j++){
+            let branch = merged_branches[j];
+            $select.append("<option value='"+branch+"'>"+branch+"</option>");
         }
     }
 }
--- docs/_static/measurements.js
+++ docs/_static/measurements.js
@@ -1,11 +1,20 @@
-// Measurements for a branch
-var measurements = {};
+// Measurements for each branch
+let measurements = [
+    {}, // set 1
+    {}, // set 2
+    {}, // set3
+];
 
 // BoxChart for each measurement
-var boxCharts = {};
+let boxCharts = {};
 
 // ScatterChart for each measurement
-var scatterCharts = {};
+let scatterCharts = {};
+
+let loadingBranches = [];
+
+const mergedColor = "#2f7ed8";
+const branchColor = "#910000";
 
 // Measurements that are plotted
 const plotMeasurements = [
@@ -66,42 +75,75 @@ const xColLookup = {
 const whiskerFactor = 1.5;
 
 
-function get_branch_measurements(set_id, branch, callback){
-    const params = {
-        TableName: "scarlet_set"+set_id,
-        KeyConditionExpression: "#br = :bname",
-        ExpressionAttributeNames: {
-            "#br": "branch",
-        },
-        ExpressionAttributeValues: {
-            ":bname": branch,
-        }
-    }
-
-    docClient.query(params, function(err, data){
-        if (err) {
-            console.log(err);
-        } else {
-            // Store the measurements
-            measurements[branch] = data["Items"];
-            console.log(branch+" measurements", measurements[branch]);
-            callback();
-        }
+// When the add button is clicked, add the currently selected branch to the plots
+function onClickAdd(){
+    let branch = $("#select-branch").val();
+    getBranchMeasurements(branch, function() {
+        onLoadMeasurements(branch);
     });
 }
 
 
-function initPlots(){
-    var $div = $("#div-plots");
+// Initialize the page by loading the list of all analyzed branches and
+// merged branches from AWS
+function initPage(){
+    // Load the branches that have been processed by
+    get_branches(function(){
+        // Load the branches that have been merged
+        get_merged_branches(function(){
+            // Populate the drop downs
+            initBranches(["select-branch"]);
+            // Initialize the plots
+            initPlots();
+        });
+    });
+}
 
-    for(var i=0; i<plotMeasurements.length; i++){
-        var measurement = plotMeasurements[i];
-        var $measDiv = $("<div id='div-"+measurement+"' class=meas-div></div>");
-        var $boxFig = $("<figure class='highcharts-figure'></figure>");
-        var $boxPlot = $("<div id='div-"+measurement+"-box'></div>");
-        var $scatterFig = $("<figure class='highcharts-figure'></figure>");
-        var $scatterPlot = $("<div id='div-"+measurement+"-scatter'></div>");
-        var $imgDiv = $("<div id='#'></div>");
+
+function getBranchMeasurements(branch, callback){
+    let set_id = parseInt($("#select-dataset").val());
+    if(!([1,2,3].includes(set_id))){
+        alert("You must choose a valid dataset to plot");
+    }
+
+    // Only load measurements that haven't already been loaded
+    if(!(branch in measurements[set_id-1])){
+        const params = {
+            TableName: "scarlet_set"+set_id,
+            KeyConditionExpression: "#br = :bname",
+            ExpressionAttributeNames: {
+                "#br": "branch",
+            },
+            ExpressionAttributeValues: {
+                ":bname": branch,
+            }
+        }
+
+        docClient.query(params, function(err, data){
+            if (err) {
+                console.log(err);
+            } else {
+                // Store the measurements
+                measurements[set_id-1][branch] = data["Items"];
+                callback(branch);
+            }
+        });
+    } else {
+        callback(branch);
+    }
+}
+
+
+function initPlots(){
+    let $div = $("#div-plots");
+    for(let i=0; i<plotMeasurements.length; i++){
+        let measurement = plotMeasurements[i];
+        let $measDiv = $("<div id='div-"+measurement+"' class=meas-div></div>");
+        let $boxFig = $("<figure class='highcharts-figure'></figure>");
+        let $boxPlot = $("<div id='div-"+measurement+"-box'></div>");
+        let $scatterFig = $("<figure class='highcharts-figure'></figure>");
+        let $scatterPlot = $("<div id='div-"+measurement+"-scatter'></div>");
+        let $imgDiv = $("<div id='#'></div>");
 
         // Add the container for these measurements to the main div
         $div.append($measDiv);
@@ -122,50 +164,13 @@ function initPlots(){
 }
 
 
-function getBoxPlotData(branch, measurement){
-    var meas = [];
-    for(var i=0; i<measurements[branch].length; i++){
-        meas.push(getYData(measurements[branch][i], measurement));
-    }
-    meas.sort(function(a, b){return a-b});
-
-    var median = Math.round(meas.length/2);
-    var idx = Math.floor(meas.length/4);
-
-    var q1 = meas[idx];
-    var q3 = meas[meas.length-idx];
-    var whisker = whiskerFactor *  (q3-q1);
-    var lowWhisker = Math.max(q1-whisker, meas[0]);
-    var highWhisker = Math.min(q3+whisker, meas[meas.length-1]);
-
-    var data = {
-        low: lowWhisker,
-        q1: meas[idx],
-        median: meas[median],
-        q3: meas[meas.length-idx],
-        high: highWhisker,
-    }
-
-
-    var outliers = [];
-    for(var i=0; i<meas.length; i++){
-        var observation = meas[i];
-        if(observation<lowWhisker || observation>highWhisker){
-            outliers.push(observation);
-        }
-    }
-    return {
-        measurements: data,
-        outliers: outliers,
-    };
-}
-
 function initBoxChart(measurement){
     boxCharts[measurement] = new Highcharts.chart("div-"+measurement+"-box", {
         chart: {
             type: 'boxplot',
             plotBorderColor: "#888888",
             plotBorderWidth: 2,
+            zoomType: "y",
         },
         title: {
             text: measurement,
@@ -174,11 +179,13 @@ function initBoxChart(measurement){
             enabled: false
         },
         xAxis: {
-            categories: [],
             title: {
                 text: 'Branch'
             },
             gridLineWidth: 0,
+            labels: {
+                rotation: -45,
+            },
         },
         yAxis: {
             title: {
@@ -208,6 +215,7 @@ function initScatterChart(measurement){
             enabled: false
         },
         xAxis: {
+            categories: [],
             title: {
                 text: xUnit
             },
@@ -246,8 +254,163 @@ function initScatterChart(measurement){
 }
 
 
+function getBoxSeries(data){
+    return {
+        name: "Measurements",
+        data: data,
+        events: {
+            click: function(event){
+                populateScatterPlot(event.point.branch);
+            }
+        },
+    }
+}
+
+
+function getOutlierSeries(data, branch){
+    return {
+        name: branch + " outliers",
+        type: "scatter",
+        data: data["outliers"],
+    }
+}
+
+
+function onLoadMeasurements(branch){
+    for(let i=0; i<plotMeasurements.length; i++){
+        let measurement = plotMeasurements[i];
+        let data = getBoxPlotData(branch, measurement, branchColor);
+        let chart = boxCharts[measurement];
+
+        // Add measurements to the plot
+        let categories = chart.xAxis[0].categories.slice();
+        categories.push(branch);
+        chart.xAxis[0].setCategories(categories);
+        chart.series[0].addPoint(data["measurements"], true);
+        console.log("measurement", data["measurements"]);
+    }
+
+    // Update the scatter plots with the current branch
+    populateScatterPlot(branch);
+}
+
+
+function onLoadMergedMeasurements(branch){
+    let datapoints = Math.min(merged_branches.length, parseInt($("#input-history").val()));
+    let idx = loadingBranches.indexOf(branch);
+    if(idx >= 0){
+        loadingBranches.splice(idx, 1);
+    }
+
+    if(loadingBranches.length === 0){
+        for(let i=0; i<plotMeasurements.length; i++){
+            let measurement = plotMeasurements[i];
+            let chart = boxCharts[measurement];
+            let categories = [];
+            let series = [];
+
+            for(let j=datapoints-1; j>=0; j--){
+                let _branch = merged_branches[j];
+                let data = getBoxPlotData(_branch, measurement, mergedColor);
+                categories.push(_branch);
+                series.push(data["measurements"]);
+            }
+            chart.addSeries(getBoxSeries(series));
+
+            // Set the x-axis for the chart
+            chart.xAxis[0].setCategories(categories);
+        }
+    }
+}
+
+
+function getBoxPlotData(branch, measurement, color){
+    let meas = [];
+    let set_id = $("#select-dataset").val();
+    for(let i=0; i<measurements[set_id-1][branch].length; i++){
+        meas.push(getYData(measurements[set_id-1][branch][i], measurement));
+    }
+    meas.sort(function(a, b){return a-b});
+
+    let median = Math.round(meas.length/2);
+    let idx = Math.floor(meas.length/4);
+
+    let q1 = meas[idx];
+    let q3 = meas[meas.length-idx];
+    let whisker = whiskerFactor *  (q3-q1);
+    let lowWhisker = Math.max(q1-whisker, meas[0]);
+    let highWhisker = Math.min(q3+whisker, meas[meas.length-1]);
+
+    let chart = boxCharts[measurement];
+    let x = 0;
+    if(chart.series.length>0){
+        x = chart.series[0].data.length;
+    }
+    let datapoints = Math.min(merged_branches.length, parseInt($("#input-history").val()));
+    if(merged_branches.includes(branch)){
+        x = datapoints-1 - merged_branches.indexOf(branch);
+    }
+
+    let data = {
+        low: lowWhisker,
+        q1: meas[idx],
+        median: meas[median],
+        q3: meas[meas.length-idx],
+        high: highWhisker,
+        x: x,
+        branch: branch,
+        color: color,
+    }
+
+
+    let outliers = [];
+    for(let i=0; i<meas.length; i++){
+        let observation = meas[i];
+        if(observation<lowWhisker || observation>highWhisker){
+            outliers.push([x, observation]);
+        }
+    }
+
+    return {
+        measurements: data,
+        outliers: outliers,
+    };
+}
+
+
+// Populate the box plot with measurements from merged branches
+function addMergedMeasurements(){
+    // First clear all of the plot data
+    for(let i=0; i<plotMeasurements.length; i++){
+        let chart = boxCharts[plotMeasurements[i]];
+        while(chart.series.length > 0)
+            chart.series[0].remove(true);
+    }
+
+    let datapoints = Math.min(merged_branches.length, parseInt($("#input-history").val()));
+    for(let i=datapoints-1; i>=0; i--){
+        let branch = merged_branches[i];
+        loadingBranches.push(branch);
+        getBranchMeasurements(branch, function() {
+            onLoadMergedMeasurements(branch);
+        });
+    }
+}
+
+
+function populateScatterPlot(branch){
+    for(let j=0; j<plotMeasurements.length; j++){
+        let measurement = plotMeasurements[j];
+        let data = getScatterData(branch, measurement);
+        let scatter = scatterCharts[measurement];
+        scatter.series[0].setData(data);
+        scatter.setTitle({text:measurement+": "+branch});
+    }
+}
+
+
 function getYData(measData, measName){
-    let y = 0;
+    let y;
     if(measName.includes("diff")){
         let lhs = measData[measName[0]+" truth"];
         let rhs = measData[measName[0]+" mag"]
@@ -260,7 +423,8 @@ function getYData(measData, measName){
 
 
 function getScatterData(branch, measurement){
-    let meas = measurements[branch];
+    let set_id = $("#select-dataset").val();
+    let meas = measurements[set_id-1][branch];
     let result = [];
     for(let i=0; i<meas.length; i++){
         let x = meas[i][xColLookup[measurement]];
@@ -281,7 +445,7 @@ function getScatterData(branch, measurement){
 
 
 function textToClipboard(text) {
-    var dummy = document.createElement("textarea");
+    let dummy = document.createElement("textarea");
     document.body.appendChild(dummy);
     dummy.value = text;
     dummy.select();
