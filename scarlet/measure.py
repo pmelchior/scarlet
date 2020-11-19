063a951fa50c7afd5f9a3ffe5a4651bafa966aa2
Peter Melchior <peter.m.melchior@gmail.com>
2020-11-19 15:52:49 -0500

    snr also works with convolved obs

--- scarlet/measure.py
+++ scarlet/measure.py
@@ -67,25 +67,46 @@ def snr(component, observations, prerender=True):
 
     observations: `scarlet.Observation` or list thereof
     """
+    if not hasattr(observations, "__iter__"):
+        observations = (observations,)
+
     if hasattr(component, "get_model"):
-        model = component.get_model()
+        frame = None
+        if not prerender:
+            frame = observations[0].model_frame
+        model = component.get_model(frame=frame)
         bbox = component.bbox
     else:
         model = component
         bbox = Box(model.shape)
 
-    C = model.shape[0]
-    M = model.reshape(C, -1)
-    # weights are given by normalized model
-    W = M / M.sum(axis=1)[:, None]
+    if prerender:
+        C = model.shape[0]
+        M = model.reshape(C, -1)
+        # weights are given by normalized model
+        W = M / M.sum(axis=1)[:, None]
 
-    # compute SNR for this component
-    detect_all, std_all = initialization.build_detection_image(
-        observations, prerender=prerender
-    )
-
-    boxed_std = bbox.extract_from(std_all)
-    var = (boxed_std ** 2).reshape(C, -1)
+        # get variance of the deconvolved coadd
+        detect_all, std_all = scarlet.initialization.build_detection_image(
+            observations, prerender=prerender
+        )
+        boxed_std = bbox.extract_from(std_all)
+        var = (boxed_std ** 2).reshape(C, -1)
+    else:
+        M = []
+        W = []
+        var = []
+        # convolve model for every observation;
+        # flatten in channel direction because it may not have all C channels; concatenate
+        # do same thing for noise variance
+        for obs in observations:
+            model_ = obs.render(model)
+            M.append(model_.reshape(-1))
+            W.append((model_ / (model_.sum(axis=(-2, -1))[:, None, None])).reshape(-1))
+            var.append(1 / obs.weights.reshape(-1))
+        M = np.concatenate(M)
+        W = np.concatenate(W)
+        var = np.concatenate(var)
 
     # SNR from Erben (2001), eq. 16, extended to multiple bands
     # SNR = (I @ W) / sqrt(W @ Sigma^2 @ W)
